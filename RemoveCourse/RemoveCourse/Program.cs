4c7046a1e944b8df92e3756f8464efb65137274d
PFC-webprojects <33673932+PFC-webprojects@users.noreply.github.com>
2018-06-08 20:04:52 -0700

    Class Coding Individual

---
+++ RemoveCourse/RemoveCourse/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoveCourse
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Database db = new Database();
+            View vw = new View();
+            CertificateProgram cp = new CertificateProgram(db, vw);
+        }
+
+    }
+}
